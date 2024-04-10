b270603acb91e1c06122d977906eff3563dcfca3
Ryan Cokely <ryan.cokely@gmail.com>
2024-04-10 14:09:33 -0400

    homepage

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './client/components/HomeScreen'; // Import the Home screen component
-import GameBoardScreen from './client/components/GameBoardScreen'; // Import the GameBoard screen component
+// // import React from 'react';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import HomeScreen from './client/components/HomeScreen'; // Import the Home screen component
+// import GameBoardScreen from './client/components/GameBoardScreen'; // Import the GameBoard screen component
 
-const Stack = createStackNavigator();
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Cinema Connection' }} />
-        <Stack.Screen name="GameBoard" component={GameBoardScreen} options={{ title: 'Game Board' }} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+// const Stack = createStackNavigator();
+
+// // export default function App() {
+// //   return (
+// //     <NavigationContainer>
+// //       <Stack.Navigator initialRouteName="Home">
+// //         <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Cinema Connection' }} />
+// //         <Stack.Screen name="GameBoard" component={GameBoardScreen} options={{ title: 'Game Board' }} />
+// //       </Stack.Navigator>
+// //     </NavigationContainer>
+// //   );
+// // }
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
 
 // const styles = StyleSheet.create({
 //   container: {
@@ -25,3 +38,29 @@ export default function App() {
 //     justifyContent: 'center',
 //   },
 // });
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import GameBoardScreen from './client/components/GameBoardScreen';
+import Test from './client/components/Test';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* <Text>Open up App.js to start working on your app!!</Text> */}
+      <StatusBar style="auto" />
+      <GameBoardScreen/>
+      <Test/>
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
